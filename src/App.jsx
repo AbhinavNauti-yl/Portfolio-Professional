c6718232ad58db6e7224b4cfe26a2c060994df62
abhinav <abhinavnautiyal2004@gmail.com>
2026-08-23 23:53:27 +0530

    Implement section highlighting and smooth scrolling in Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,36 @@ import Certifications from "./pages/Certifications";
 import Contact from "./pages/Contact";
 
 import { Toaster } from "react-hot-toast";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [activeSection, setActiveSection] = useState();
+  useEffect(() => {
+    const sections = document.querySelectorAll("section");
+
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            setActiveSection(entry.target.id)
+          }
+        });
+      },
+      {
+        threshold: 0.5,
+      }
+    );
+
+    sections.forEach((section) => observer.observe(section));
+
+    return () => observer.disconnect();
+  }, []);
   return (
-      <ThemeProvider>
-        <Router>
-          <div className="min-h-screen bg-gray-50 dark:bg-[#151616] transition-colors duration-300">
-            <Navbar />
-            {/* <main>
+    <ThemeProvider>
+      <Router>
+        <div className="min-h-screen bg-gray-50 dark:bg-[#151616] transition-colors duration-300">
+          <Navbar activeSection={activeSection} setActiveSection={setActiveSection} />
+          {/* <main>
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/about" element={<About />} />
@@ -26,16 +48,41 @@ function App() {
               <Route path="/contact" element={<Contact />} />
             </Routes>
           </main> */}
+          {/* <Home />
+          <About />
+          <Skills />
+          <Projects />
+          <Certifications />
+          <Contact /> */}
+
+          <section id="home">
             <Home />
+          </section>
+
+          <section id="about">
             <About />
+          </section>
+
+          <section id="skills">
             <Skills />
+          </section>
+
+          <section id="projects">
             <Projects />
+          </section>
+
+          <section id="certifications">
             <Certifications />
+          </section>
+
+          <section id="contact">
             <Contact />
-          </div>
-        </Router>
-        <Toaster />
-      </ThemeProvider>
+          </section>
+          
+        </div>
+      </Router>
+      <Toaster />
+    </ThemeProvider>
   );
 }
 
